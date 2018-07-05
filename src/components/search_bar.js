5cab2870f2aae3b0ea4fcf99e3230bdba2b41363
Alex Sell <alextricityy@gmail.com>
2018-07-05 17:51:38 +0100

    cleaned up code

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,7 +4,7 @@ class SearchBar extends Component {
   constructor(props){
     super(props);
 
-    this.state= {term: ""};
+    this.state= {term: ''};
 }
 
 render(){
@@ -18,13 +18,10 @@ render(){
   );
 }
 
-// onInputChange(event) {
-// console.log(event);
-// }
   onInputChange(term){
     this.setState({term});
     this.props.onSearchTermChange(term);
   }
-};
+}
 
 export default SearchBar;
